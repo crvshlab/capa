754f8d0f7179cb34363a94bd542d8b29e31d07f1
Alex Salom <alex.salom@gmail.com>
2018-12-18 15:15:22 +0100

    can record fully tested

--- spec/emulator_recorder_spec.rb
+++ spec/emulator_recorder_spec.rb
@@ -17,9 +17,41 @@ describe EmulatorRecorder do
   describe 'can record' do
     context 'when adb is not available' do
       it 'should return false' do
-        allow(@sut).to receive(:command?).with('adb').and_return(true)
+        allow(@sut).to receive(:command?).with('adb').and_return(false)
 
         expect(@sut.can_record?).to be == false
+      end
+    end
+
+    context 'when adb is available' do
+      context 'and no devices are returned' do
+        it 'should return false' do
+          allow(@sut).to receive(:command?).with('adb').and_return(true)
+
+          allow(@sut).to receive(:`).with("adb devices -l").and_return("")
+
+          expect(@sut.can_record?).to be == false
+        end
+      end
+
+      context 'and two devices are returned' do
+        it 'should return true' do
+          allow(@sut).to receive(:command?).with('adb').and_return(true)
+
+          allow(@sut).to receive(:`).with("adb devices -l").and_return("device1\ndevice2")
+
+          expect(@sut.can_record?).to be == true
+        end
+      end
+
+      context 'and three devices are returned' do
+        it 'should return false' do
+          allow(@sut).to receive(:command?).with('adb').and_return(true)
+
+          allow(@sut).to receive(:`).with("adb devices -l").and_return("device1\ndevice2\ndevice3")
+
+          expect(@sut.can_record?).to be == false
+        end
       end
     end
   end
