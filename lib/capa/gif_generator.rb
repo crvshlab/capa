948782d21fac9da57c056516b78a97efa9f22541
Alex Salom <alex.salom@gmail.com>
2018-12-18 15:00:23 +0100

    gif generator tests

--- lib/capa/gif_generator.rb
+++ lib/capa/gif_generator.rb
@@ -1,32 +1,34 @@
 require_relative 'string'
 
-class GIFGenerator
-  def initialize(input_video: '', output_gif: '')
-    abort('Please provide a video to generate a GIF from') if input_video.blank?
-    abort('Please provide a name for the GIF') if output_gif.blank?
-    abort("The names for the video and the GIF can't be the same") if output_gif == input_video
-    @input_video = input_video
-    @output_gif = output_gif
-  end
-
-  def generate
-    abort("File not found: #{@input_video}") unless File.exists?(@input_video)
-    puts "Generating GIF, please wait..."
-    `gifify #{@input_video} -o #{@output_gif} --resize 300:-1 --colors 220 --fps 7`
-    output_directory = Dir.pwd
-    video_path = "#{output_directory}/#{@input_video}"
-    gif_path = "#{output_directory}/#{@output_gif}"
-    
-    if File.exists?(video_path)
-      puts "Video: #{video_path}"
-    else
-      puts "Error generating video"
+# module Capa
+  class GIFGenerator
+    def initialize(input_video: '', output_gif: '')
+      abort('Please provide a video to generate a GIF from') if input_video.blank?
+      abort('Please provide a name for the GIF') if output_gif.blank?
+      abort("The names for the video and the GIF can't be the same") if output_gif == input_video
+      @input_video = input_video
+      @output_gif = output_gif
     end
 
-    if File.exists?(gif_path)
-      puts "GIF: #{gif_path}"
-    else
-      puts "Error generating GIF"
+    def generate
+      abort("File not found: #{@input_video}") unless File.exists?(@input_video)
+      puts "Generating GIF, please wait..."
+      `gifify #{@input_video} -o #{@output_gif} --resize 300:-1 --colors 220 --fps 7`
+      output_directory = Dir.pwd
+      video_path = "#{output_directory}/#{@input_video}"
+      gif_path = "#{output_directory}/#{@output_gif}"
+      
+      if File.exists?(video_path)
+        puts "Video: #{video_path}"
+      else
+        puts "Error generating video"
+      end
+
+      if File.exists?(gif_path)
+        puts "GIF: #{gif_path}"
+      else
+        puts "Error generating GIF"
+      end
     end
   end
-end
+# end
--- spec/gif_generator_spec.rb
+++ spec/gif_generator_spec.rb
@@ -1,8 +1,12 @@
 require_relative "../lib/capa/gif_generator"
 
 describe GIFGenerator do
+  # include Capa
+
   before(:each) do
-    @sut = GIFGenerator.new(input_video: 'video', output_gif: 'gif')
+    @video_file = 'video'
+    @gif_file = 'gif'
+    @sut = GIFGenerator.new(input_video:  @video_file, output_gif: @gif_file)
   end
   describe 'when initializing an object' do
     context 'when the parameters are invalid' do
@@ -10,6 +14,26 @@ describe GIFGenerator do
         expect { GIFGenerator.new(input_video: '', output_gif: 'something') }.to raise_error(SystemExit)
         expect { GIFGenerator.new(input_video: 'something', output_gif: '') }.to raise_error(SystemExit)
         expect { GIFGenerator.new(input_video: 'something', output_gif: 'something') }.to raise_error(SystemExit)
+      end
+    end
+  end
+
+  describe 'when generator a gif' do
+    context "video file doesn't exist" do
+      it 'should abort' do
+        allow(File).to receive(:exists?).with(@video_file).and_return(false)
+
+        expect { @sut.generate }.to raise_error(SystemExit)
+      end
+    end
+
+    context 'video file does exist' do
+      it 'should make a call to gififi' do
+        allow(File).to receive(:exists?).and_return(true)
+  
+        expect(@sut).to receive(:`).with("gifify #{@video_file} -o #{@gif_file} --resize 300:-1 --colors 220 --fps 7")
+        
+        @sut.generate
       end
     end
   end
