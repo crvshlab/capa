1e2e9a0bf97a102a4192bacfde858f873193537b
Alex Salom <alex.salom@gmail.com>
2018-12-18 11:48:50 +0100

    recorder factory tests

--- spec/recorder_factory_spec.rb
+++ spec/recorder_factory_spec.rb
@@ -4,9 +4,17 @@ describe RecorderFactory do
   before(:each) do
     @sut = RecorderFactory.new(filename: 'filename')
   end
+
+  describe 'factory initializer' do
+    it 'crates the right objects' do
+      filename = 'a_filename'
+      expect(SimulatorRecorder).to receive(:new).with(filename: filename)
+      expect(EmulatorRecorder).to receive(:new).with(filename: filename)
+      RecorderFactory.new(filename: filename)
+    end
+  end
   
   describe 'create platform' do
-
     context 'given ios platform is provided' do
       it 'returns an object of type SimulatorRecorder' do
         expect(@sut.create(from_platform: 'ios')).to be_a(SimulatorRecorder)
@@ -21,7 +29,54 @@ describe RecorderFactory do
 
     context 'given any other platform is provided' do
       it 'returns nil' do
-        expect(@sut.create(from_platform: 'another')).to be_nil()
+        expect(@sut.create(from_platform: 'any other')).to be_nil()
+      end
+    end
+  end
+
+  describe 'infer platform' do
+    before(:each) do
+      @simulator_recorder = double('simulator_recorder')
+      @emulator_recorder = double('emulator_recorder')
+      allow(SimulatorRecorder).to receive(:new).and_return(@simulator_recorder)
+      allow(EmulatorRecorder).to receive(:new).and_return(@emulator_recorder)
+
+      @sut = RecorderFactory.new(filename: 'filename')
+    end
+
+    context 'when no platforms can record' do
+      it 'should not infer the platform' do
+        allow(@simulator_recorder).to receive(:can_record?).and_return(false)
+        allow(@emulator_recorder).to receive(:can_record?).and_return(false)
+
+        expect(@sut.infer_platform).to be_nil()
+      end
+    end
+
+    context 'when ios platform can record' do
+      it 'should infer that platform' do
+        allow(@simulator_recorder).to receive(:can_record?).and_return(true)
+        allow(@emulator_recorder).to receive(:can_record?).and_return(false)
+
+        expect(@sut.infer_platform).to be == 'ios'
+      end
+    end
+
+    context 'when android platform can record' do
+      it 'should infer that platform' do
+        allow(@simulator_recorder).to receive(:can_record?).and_return(false)
+        allow(@emulator_recorder).to receive(:can_record?).and_return(true)
+
+        expect(@sut.infer_platform).to be == 'android'
+      end
+    end
+
+    context 'when all platform can record' do
+      it 'should not infer the platform' do
+        allow(@simulator_recorder).to receive(:can_record?).and_return(true)
+        allow(@emulator_recorder).to receive(:can_record?).and_return(true)
+
+        expect(@sut.infer_platform).to be_nil()
       end
     end
   end
